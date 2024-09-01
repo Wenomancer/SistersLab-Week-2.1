e185bec92047959f4ae1f03721e35c5531785fc3
Wenomancer <cakmakabdulsamet@gmail.com>
2024-09-01 09:39:28 +0300

    post model created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+import 'package:logger/logger.dart';
+import 'package:sisterslabsecond/model/post_model.dart';
 
 void main() {
   runApp(MyView());
@@ -14,30 +19,64 @@ class MyView extends StatefulWidget {
 }
 
 class _MyViewState extends State<MyView> {
-  String data = "Loading...";
+  String title = "Loading...";
+  String body = "Loading...";
+  int userId = 8888888;
+  int id = 99999999;
 
   @override
   Widget build(BuildContext context) {
-
-    myFunction().then((value) {
-      setState(() {
-        data = value;
-      });
+    testApi().then((value) {
+      if (value != null) {
+        setState(() {
+          title = value[0].title;
+          body = value[0].body;
+          userId = value[0].userId;
+          id = value[0].id;
+        });
+      }
     });
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-        child: Text(data),
-)
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(title),
+            Text(body),
+            Text(userId.toString()),
+            Text(id.toString()),
+          ],
+        ),
       ),
     );
   }
 }
 
+Future<List<PostModel>?> testApi() async {
+  List<PostModel> posts = [];
 
-Future<String> myFunction() async {
-  await Future.delayed(Duration(seconds: 5));
-  return "Hello";
+  try {
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      posts = (convert.jsonDecode(response.body) as List)
+          .map((data) => PostModel.fromJson(data))
+          .toList();
+
+      Logger().d(posts[0].title);
+      return posts;
+
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+      Logger().d('Request failed with status: ${response.statusCode}.');
+    }
+  } on FormatException catch (e) {
+    print('FormatException caught: $e');
+    Logger().e('FormatException caught: $e');
+  } catch (e) {
+    Logger().e('Exception: $e');
+  }
+  return null;
 }
