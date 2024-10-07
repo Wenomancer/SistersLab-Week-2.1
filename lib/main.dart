bc8c96e3ddb0fb6f3b83119ca911182ea5b18605
Wenomancer <cakmakabdulsamet@gmail.com>
2024-10-07 23:06:48 +0300

    push notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sisterslabsecond/add_task/presentation/add_task_page.dart';
@@ -14,6 +15,14 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  FirebaseMessaging.onBackgroundMessage(
+    (message) {
+      // Arka planda bildirim geldiğinde bu fonksiyon çalışır.
+      print("Message in the background: ${message.messageId}");
+      return Future<void>.value();
+    },
+  );
+
   runApp(
     ChangeNotifierProvider(
       create: (context) {
@@ -24,8 +33,47 @@ Future<void> main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseMessaging.instance.requestPermission();
+
+    FirebaseMessaging.instance.getToken().then((token) {
+      print("FCM Token: $token");
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Message received: ${message.notification?.title}');
+      if (message.notification != null && mounted) {
+        showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: Text(message.notification!.title ?? 'No Title'),
+            content: Text(message.notification!.body ?? 'No Body'),
+          ),
+        );
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A notification was opened: ${message}');
+      if (message.data['route'] != null) {
+        Navigator.of(context).pushNamed(message.data['route']); // Rotaya git
+      }
+    });
+  }
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
